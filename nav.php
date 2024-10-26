ca9d6d39b0905c1e337005a0d204eac6afd40365
Allam <92165435+mahmoud530@users.noreply.github.com>
2024-10-26 17:48:20 +0300

    Update nav.php

--- nav.php
+++ nav.php
@@ -35,50 +35,59 @@ include("connection.php");
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
 
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Sportigo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link" aria-current="page" href="Home.php">Home</a>
-                <a class="nav-link" href="all_products.php">Products</a>
-                <a class="nav-link" href="#category">Categories</a>
-                <a class="nav-link" href="#offer">Offers</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Homee.php">Sportigo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="Homee.php">Home</a>
+                    <a class="nav-link" href="all-products.php">Products</a>
+                    <a class="nav-link" href="#category">Categories</a>
+                    <a class="nav-link" href="#offer">Offers</a>
+                </div>
             </div>
+
+            <!-- SEARCH -->
+                
+            <form method="POST" class="d-flex" id="searchForm" onsubmit="search(); return false;">
+                <input class="form-control me-2" onkeyup="search()" type="search" placeholder="Search" name="text" aria-label="Search"
+                    id="searchInput"> 
+            </form>
+            <form method="POST">
+
+            <?php if (empty($_SESSION) || !isset($_SESSION['user_id'])) { ?>
+                <a href="login and sign up.php">
+                    <button type="button" class="btn-login">
+            Login
+        </button>
+                    </a>
+  
+<?php } else { ?>
+    <button class="btn-login "name="logout">
+        Logout
+    </button>
+<?php } ?>
+</from>
+            
+            <div class="icons collapse navbar-collapse" id="navbarNavAltMarkup">
+                <a href="cart.php">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+                <a href="wishlist.php">
+                    <i class="fa-solid fa-heart"></i>
+                </a>
+                <a href="profile.php">
+                    <i class="fa-solid fa-user"></i>
+                </a>
+            </div>
+
         </div>
-
-        <!-- SEARCH -->
-        <form method="POST" class="d-flex" id="searchForm" onsubmit="search(); return false;">
-            <input class="form-control me-2" onkeyup="search()" type="search" placeholder="Search" name="text" aria-label="Search"
-                id="searchInput"> 
-        </form>
-
-        <form method="POST">
-            <?php if (isset($_SESSION['user_id'])) { ?>
-                <button class="btn-login" name="logout">Logout</button>
-            <?php } else { ?>
-                <button class="btn-login" name="login">Login</button>
-            <?php } ?>
-        </form>
-
-        <!-- ICONS -->
-        <div class="icons collapse navbar-collapse" id="navbarNavAltMarkup">
-            <a href="cart.php">
-                <i class="fa-solid fa-cart-shopping"></i>
-            </a>
-            <a href="wishlist.php">
-                <i class="fa-solid fa-heart"></i>
-            </a>
-            <a href="profile.php">
-                <i class="fa-solid fa-user"></i>
-            </a>
-        </div>
-
-    </div>
-</nav>
-
+    </nav>
+    
 </body>
 </html>
+
+    
